436eface603689d02b2e58f9b43eafbaeda175be
Cole <Cole@CCMBP1.local>
2016-08-30 17:57:00 +0800

    Update express sample

--- express_sample.js
+++ express_sample.js
@@ -10,12 +10,92 @@ app.get('/',function(req, res){
     res.send("Hello World");
 });
 
+//Ignore request method
+//In the following example, the handler will be executed for requests to “/secret”
+//whether you are using GET, POST, PUT, DELETE, or any other HTTP request method that is supported in the http module.
+app.all('/secret', function (req, res, next) {
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
+});
+
 //Single Static File
 //If you have a specific file, use the res.sendFile() function.
 //If you are serving many assets from a directory, use the express.static() middleware function.
 app.get('/plainHtml', function(req, res){
     res.sendFile(__dirname+'/plainHtml.html');
 });
+
+
+
+//Route Parameters
+//The name of route parameters must be made up of “word characters” ([A-Za-z0-9_]).
+//Route path: /users/:userId/books/:bookId
+//Request URL: http://localhost:3000/users/34/books/8989
+//req.params: { "userId": "34", "bookId": "8989" }
+app.get('/users/:userId/books/:bookId', function(req, res) {
+    res.send(req.params);
+});
+
+//Special Route Parameters
+//Route path: /flights/:from-:to
+//Request URL: http://localhost:3000/flights/LAX-SFO
+//req.params: { "from": "LAX", "to": "SFO" }
+app.get('/flights/:from-:to', function(req, res){
+    res.send(req.params);
+});
+//Route path: /plantae/:genus.:species
+//Request URL: http://localhost:3000/plantae/Prunus.persica
+//req.params: { "genus": "Prunus", "species": "persica" }
+app.get('/plantae/:genus.:species', function(req, res){
+    res.send(req.params);
+});
+
+
+
+
+//More than one callback function
+app.get('/example/b', function (req, res, next) {
+    console.log('the response will be sent by the next function ...');
+    next();
+}, function (req, res) {
+    res.send('Hello from B!');
+});
+
+//A combination of independent functions and arrays of functions can handle a route
+var cb0 = function (req, res, next) {
+    console.log('CB0');
+    next();
+}
+var cb1 = function (req, res, next) {
+    console.log('CB1');
+    next();
+}
+app.get('/example/d', [cb0, cb1], function (req, res, next) {
+    console.log('the response will be sent by the next function ...');
+    next();
+}, function (req, res) {
+    res.send('Hello from D!');
+});
+
+//Chained route handlers
+app.route('/book')
+    .get(function(req, res) {
+        res.send('Get a random book');
+    })
+    .post(function(req, res) {
+        res.send('Add a book');
+    })
+    .put(function(req, res) {
+        res.send('Update the book');
+    });
+
+//The following example creates a router as a module, loads a middleware function in it,
+//defines some routes, and mounts the router module on a path in the main app.
+//Example: The app will now be able to handle requests to /birds and /birds/about,
+//as well as call the timeLog middleware function that is specific to the route.
+var birds = require('./birds');
+app.use('/birds', birds);
+
 
 //Handle 404 Response
 //In Express, 404 responses are not the result of an error, so the error-handler middleware will not capture them.
